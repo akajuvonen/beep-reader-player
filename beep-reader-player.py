c9234bdf04d86cc5331f42d9251baeb559ee036d
Antti Juvonen <akajuvonen@users.noreply.github.com>
2016-04-01 23:26:46 +0300

    Added docstrings and comments

--- beep-reader-player.py
+++ beep-reader-player.py
@@ -3,11 +3,28 @@ import numpy as np
 
 # Add the data points to an array (create the wave)
 def create_wave(bits,sampling_rate,volume,freq,duration):
+    """Creates an array of sine wave values based on certain parameters.
+    Arguments:
+    bits -- How many bits are used in the values, e.g., 16
+    sampling_rate -- How many samples per second, e.g., 44100
+    volume -- Volume, from 0 to 1 (max)
+    freq -- The frequency of the note
+    duration -- Duration of the note in seconds
+    Returns:
+    snd_array -- Array of the notes sine wave values, as 16-bit numpy integers
+    """
+    # Calculate the volume based on the used bit value (signed int)
     volume = pow(2,bits-1)-1
+    # An empty list
     snd_list = []
+    # Fill the list with sine wave values
     for x in range(0,int(sampling_rate*duration)):
         value = volume * np.sin(2 * np.pi * freq * x / sampling_rate)
         snd_list.append(value)
+    # Create a numpy array of the list, needed later.
+    # Note: We don't create a numpy array earlier, because when
+    # appending values to it, a new array is always created.
+    # That is not efficient.
     snd_array = np.array(snd_list).astype(np.int16)
     return snd_array
 
