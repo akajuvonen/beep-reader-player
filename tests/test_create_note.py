89027866a1ac3ea5b10a2d2b654a4db8e247d509
Antti Juvonen <akajuvonen@users.noreply.github.com>
2016-04-07 17:44:49 +0300

    Added todo to tests.

--- tests/test_create_note.py
+++ tests/test_create_note.py
@@ -3,6 +3,7 @@ from beep_reader_player import create_note
 
 class CreateNoteTest(unittest.TestCase):
     """Test create_note function"""
+    # TODO: Setup
     def test_create_note(self):
         """Test creating a note"""
         note = create_note(16,8000,1.0,440,1.0)
