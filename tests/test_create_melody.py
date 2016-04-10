fbcd30152dc845dc15d002676e228ca30ffd4091
Antti Juvonen <akajuvonen@users.noreply.github.com>
2016-04-10 19:12:33 +0300

    Added some class variables to setup of create melody tests.

--- tests/test_create_melody.py
+++ tests/test_create_melody.py
@@ -5,4 +5,8 @@ class CreateMelodyTest(unittest.TestCase):
     """Test create_melody function"""
     def setUp(self):
         """Set up method for tests."""
-        pass
+        self.bits = 16
+        self.sampling_rate = 8000
+        self.volumes = [0.5,0.8,1.0]
+        self.freqs = [440,440,440]
+        self.durations = [0.5,0.5,1.0]
