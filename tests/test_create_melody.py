4bcf67190b23a49f7f8395efc6b821d4facc917d
Antti Juvonen <akajuvonen@users.noreply.github.com>
2016-04-10 19:09:39 +0300

    Added placeholder for create melody function tests.

---
+++ tests/test_create_melody.py
@@ -0,0 +1,8 @@
+import unittest
+from beep_reader_player import create_melody
+
+class CreateMelodyTest(unittest.TestCase):
+    """Test create_melody function"""
+    def setUp(self):
+        """Set up method for tests."""
+        pass
