02b506a935aeeeae4b03cb092396613f705be160
Antti Juvonen <akajuvonen@users.noreply.github.com>
2016-04-10 19:16:29 +0300

    Added create melody test.

--- tests/test_create_melody.py
+++ tests/test_create_melody.py
@@ -10,3 +10,12 @@ class CreateMelodyTest(unittest.TestCase):
         self.volumes = [0.5,0.8,1.0]
         self.freqs = [440,440,440]
         self.durations = [0.5,0.5,1.0]
+
+    def test_create_melody(self):
+        """Test the create melody function in a normal situation"""
+        # Create the melody
+        melody = create_melody(self.bits,self.sampling_rate,self.volumes,self.freqs,self.durations)
+        # The length of the melody in samples should be this
+        melody_samples = sum(self.durations) * self.sampling_rate
+        # Make sure it actually is
+        self.assertEqual(melody_samples,len(melody))
