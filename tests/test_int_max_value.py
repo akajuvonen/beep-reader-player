305e9002bf3002faed45ce0876b57585c30578a6
Antti Juvonen <akajuvonen@users.noreply.github.com>
2016-04-14 19:43:05 +0300

    Removed unneeded min values

--- tests/test_int_max_value.py
+++ tests/test_int_max_value.py
@@ -5,9 +5,7 @@ class CreateMelodyTest(unittest.TestCase):
     def setUp(self):
         self.bits = 16
         self.signed_expected_max = 32767
-        self.signed_expected_min = -32767
         self.unsigned_expected_max = 65535
-        self.unsigned_expected_min = 0
 
     def test_int_max_value_signed(self):
         signed = True
