04baeb0a3f834e2688728caef51c67b2e71d0bfb
Antti Juvonen <akajuvonen@users.noreply.github.com>
2016-04-27 12:16:54 +0300

    Added placeholder for calc note freq function tests

---
+++ tests/test_calc_note_freq.py
@@ -0,0 +1,8 @@
+import unittest
+from beep_reader_player import calculate_note_freq
+
+class CalculateFreqTest(unittest.TestCase):
+    """Test calculate_note_freq function"""
+    def setUp(self):
+        """Set up method for tests."""
+        pass
