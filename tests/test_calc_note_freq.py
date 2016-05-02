7d00c1ab9a988962aa3f3da206e0c20929afaf6f
Antti Juvonen <akajuvonen@users.noreply.github.com>
2016-05-02 19:47:42 +0300

    Removed unneeded setup function

--- tests/test_calc_note_freq.py
+++ tests/test_calc_note_freq.py
@@ -3,9 +3,6 @@ from beep_reader_player import calculate_note_freq
 
 class CalculateFreqTest(unittest.TestCase):
     """Test calculate_note_freq function"""
-    def setUp(self):
-        """Set up method for tests."""
-        pass
 
     def test_calc_note_freq(self):
         """Test for calculating note frequency"""
