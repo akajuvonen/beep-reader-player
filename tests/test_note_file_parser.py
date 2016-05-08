e8885e42a0154566b6c06e1768904b95597d6650
Antti Juvonen <akajuvonen@users.noreply.github.com>
2016-05-08 13:31:07 +0300

    Added test placeholder for note parser

---
+++ tests/test_note_file_parser.py
@@ -0,0 +1,8 @@
+import unittest
+from note_file_parser import note_file_parser
+
+class NoteFileParserTest(unittest.TestCase):
+    """Tests for note parsing from file"""
+    def parse_notes_test(self):
+        """Parse notes from a file test"""
+        pass
