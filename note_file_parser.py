a8856eca23518579904df7b3e96529eddc16f10c
Antti Juvonen <akajuvonen@users.noreply.github.com>
2016-05-10 19:12:49 +0300

    Added main function template to note file parser

--- note_file_parser.py
+++ note_file_parser.py
@@ -3,3 +3,9 @@
 def note_file_parser(filename):
     with open(filename,'r') as f:
         contents = f.read()
+
+def main():
+    filename = 'tests/notefile.txt'
+
+if __name__=='__main__':
+    main()
