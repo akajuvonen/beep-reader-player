dcf374a570f930752ead4227b5330558af9cbda9
Antti Juvonen <akajuvonen@users.noreply.github.com>
2016-05-11 17:49:24 +0300

    Added dummy return lists for now, they are placeholders

--- note_file_parser.py
+++ note_file_parser.py
@@ -3,6 +3,10 @@
 def note_file_parser(filename):
     with open(filename,'r') as f:
         contents = f.read()
+    # Dummy return for now
+    notes = ['C4','D4','E4']
+    durations = [1.0,1.0,1.0]
+    return notes,durations
 
 def main():
     filename = 'tests/notefile.txt'
