abbc2f4badaa356ff03368f85ec1895671efd302
Antti Juvonen <akajuvonen@users.noreply.github.com>
2016-05-11 17:44:53 +0300

    Added file closing to note file tests tear down method

--- tests/test_note_file_parser.py
+++ tests/test_note_file_parser.py
@@ -7,6 +7,9 @@ class NoteFileParserTest(unittest.TestCase):
         filename = 'tests/notefile.txt'
         self.f = open(filename,'r')
 
+    def tearDown(self):
+        self.f.close()
+
     def parse_notes_test(self):
         """Parse notes from a file test"""
         pass
