e4aec7df4f7ba4c7a4e6ff86a60777db757b303a
Antti Juvonen <akajuvonen@users.noreply.github.com>
2016-05-11 17:43:57 +0300

    Added note test file opening to test setup method

--- tests/test_note_file_parser.py
+++ tests/test_note_file_parser.py
@@ -3,6 +3,10 @@ from note_file_parser import note_file_parser
 
 class NoteFileParserTest(unittest.TestCase):
     """Tests for note parsing from file"""
+    def setUp(self):
+        filename = 'tests/notefile.txt'
+        self.f = open(filename,'r')
+
     def parse_notes_test(self):
         """Parse notes from a file test"""
         pass
