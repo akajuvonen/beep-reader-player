de217e71f4f72bbae2fa2c68dcdf73dfca7d97eb
Antti Juvonen <akajuvonen@users.noreply.github.com>
2016-05-11 17:48:17 +0300

    Removed setup teardown, and added first test for note file parsing (still fails)

--- tests/test_note_file_parser.py
+++ tests/test_note_file_parser.py
@@ -3,13 +3,8 @@ from note_file_parser import note_file_parser
 
 class NoteFileParserTest(unittest.TestCase):
     """Tests for note parsing from file"""
-    def setUp(self):
-        filename = 'tests/notefile.txt'
-        self.f = open(filename,'r')
-
-    def tearDown(self):
-        self.f.close()
-
     def parse_notes_test(self):
         """Parse notes from a file test"""
-        pass
+        notes,durations = note_file_parser('tests/notefile.txt')
+        self.assertEqual(notes,['C4','D4','E4'])
+        self.assertEqual(durations,[1.0,1.0,1.0])
