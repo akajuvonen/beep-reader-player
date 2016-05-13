35b456210aaf71e55f3a0fded7b9483fec36b399
Antti Juvonen <akajuvonen@users.noreply.github.com>
2016-05-13 12:11:02 +0300

    Added comments

--- note_file_parser.py
+++ note_file_parser.py
@@ -1,14 +1,20 @@
 #!/usr/bin/env python
 
 def note_file_parser(filename):
+    # Init notes and durations lists
     notes = []
     durations = []
+    # Open file and read contents to string
     with open(filename,'r') as f:
         contents = f.read()
+    # Divide string to blocks which contain 'note:duration'
     blocks = contents.split(',')
+    # Split the block into note name and duration
     for item in blocks:
         note, duration = item.split(':')
         notes.append(note)
+        # Here we must convert to float, it also automatically
+        # gets rid of line breaks at the end of lines in the file
         durations.append(float(duration))
     return notes,durations
 
