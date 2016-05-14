1da8284ecc7ba32eec4dee660fbdc70da4d0ebc0
Antti Juvonen <akajuvonen@users.noreply.github.com>
2016-05-14 18:23:38 +0300

    Added updated note parser test

--- tests/test_note_file_parser.py
+++ tests/test_note_file_parser.py
@@ -5,6 +5,7 @@ class NoteFileParserTest(unittest.TestCase):
     """Tests for note parsing from file"""
     def parse_notes_test(self):
         """Parse notes from a file test"""
-        notes,durations = note_file_parser('tests/notefile.txt')
+        notes,durations,volumes = note_file_parser('tests/notefile.txt')
         self.assertEqual(notes,['C4','D4','E4'])
         self.assertEqual(durations,[1.0,1.0,1.0])
+        self.assertEqual(volumes,[0.5,0.5,0.5])
