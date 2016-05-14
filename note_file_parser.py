ff49ff2933b78d6fb2804322871f2fdfa2734aba
Antti Juvonen <akajuvonen@users.noreply.github.com>
2016-05-14 18:27:13 +0300

    Added volume parsing

--- note_file_parser.py
+++ note_file_parser.py
@@ -1,22 +1,24 @@
 #!/usr/bin/env python
 
 def note_file_parser(filename):
-    # Init notes and durations lists
+    # Init notes and durations lists, also volumes
     notes = []
     durations = []
+    volumes = []
     # Open file and read contents to string
     with open(filename,'r') as f:
         contents = f.read()
     # Divide string to blocks which contain 'note:duration'
     blocks = contents.split(',')
-    # Split the block into note name and duration
+    # Split the block into note name, duration, volume
     for item in blocks:
-        note, duration = item.split(':')
+        note, duration, volume = item.split(':')
         notes.append(note)
         # Here we must convert to float, it also automatically
         # gets rid of line breaks at the end of lines in the file
         durations.append(float(duration))
-    return notes,durations
+        volumes.append(float(volume))
+    return notes,durations,volumes
 
 def main():
     filename = 'tests/notefile.txt'
