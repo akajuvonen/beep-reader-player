b99153bed09e7511a98e23234403356022b98adf
Antti Juvonen <akajuvonen@users.noreply.github.com>
2016-06-05 12:13:46 +0300

    Added error handling for note file parsing

--- note_file_parser.py
+++ note_file_parser.py
@@ -8,16 +8,20 @@ def note_file_parser(filename):
     # Open file and read contents to string
     with open(filename,'r') as f:
         contents = f.read()
-    # Divide string to blocks which contain 'note:duration'
-    blocks = contents.split(',')
-    # Split the block into note name, duration, volume
-    for item in blocks:
-        note, duration, volume = item.split(':')
-        notes.append(note)
-        # Here we must convert to float, it also automatically
-        # gets rid of line breaks at the end of lines in the file
-        durations.append(float(duration))
-        volumes.append(float(volume))
+    try:
+        # Divide string to blocks which contain 'note:duration'
+        blocks = contents.split(',')
+        # Split the block into note name, duration, volume
+        for item in blocks:
+            note, duration, volume = item.split(':')
+            notes.append(note)
+            # Here we must convert to float, it also automatically
+            # gets rid of line breaks at the end of lines in the file
+            durations.append(float(duration))
+            volumes.append(float(volume))
+    except:
+        # If something goes wrong
+        raise NoteFileParsingException('Error in parsing the note file. Check the format.')
     return notes,durations,volumes
 
 def main():
