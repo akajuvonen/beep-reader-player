b4e6a6e9a0391fa134e816da842ee81379191778
Antti Juvonen <akajuvonen@users.noreply.github.com>
2017-07-08 22:22:45 +0300

    Code style upgrades

--- tests/test_calc_note_freq.py
+++ tests/test_calc_note_freq.py
@@ -1,6 +1,7 @@
 import unittest
 from beep_reader_player import calculate_note_freq
 
+
 class CalculateFreqTest(unittest.TestCase):
     """Test calculate_note_freq function"""
 
@@ -8,4 +9,4 @@ class CalculateFreqTest(unittest.TestCase):
         """Test for calculating note frequency"""
         note = 'A4'
         freq = calculate_note_freq(note)
-        self.assertEqual(freq,440.0)
+        self.assertEqual(freq, 440.0)
